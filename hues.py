42f57f47a00e121248b20679866d20dc42593ac4
Joel Gustafson <joelg@mit.edu>
2024-11-06 23:46:23 -0500

    update hues.py

--- hues.py
+++ hues.py
@@ -1,6 +1,16 @@
+import os
+import sys
+import pickle
 import numpy as np
 from scipy.spatial import Voronoi
 from scipy.sparse.csgraph import shortest_path
+from scipy import sparse
+from scipy import stats
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from graph_utils import save_labels
 
 def derive_cluster_hues(labels, cluster_centers):
     """
@@ -53,7 +63,9 @@ def derive_cluster_hues(labels, cluster_centers):
     return hue_values
 
 
-def interpolate_point_hue(point, cluster_centers, cluster_hues, method='nearest_two'):
+voronoi_cache = None
+
+def interpolate_point_hue(point, cluster_centers, cluster_hues, method='voronoi_neighbors'):
     """
     Interpolate a hue value for a point based on nearby cluster hues.
 
@@ -71,10 +83,66 @@ def interpolate_point_hue(point, cluster_centers, cluster_hues, method='nearest_
     float
         Interpolated hue value (0-1)
     """
+    global voronoi_cache
+
     # Calculate distances to all cluster centers
     distances = np.linalg.norm(cluster_centers - point, axis=1)
+    if method == 'voronoi_neighbors':
+        # Create or use cached Voronoi diagram and neighbor dictionary
+        if voronoi_cache is None:
+            vor = Voronoi(cluster_centers)
+            # Create dictionary of neighbors for each center
+            neighbor_dict = {}
+            for ridge_points in vor.ridge_points:
+                i, j = ridge_points
+                if i not in neighbor_dict:
+                    neighbor_dict[i] = set()
+                if j not in neighbor_dict:
+                    neighbor_dict[j] = set()
+                neighbor_dict[i].add(j)
+                neighbor_dict[j].add(i)
+            voronoi_cache = (vor, neighbor_dict)
+        else:
+            vor, neighbor_dict = voronoi_cache
 
-    if method == 'nearest_two':
+        # Find nearest cluster center
+        distances = np.linalg.norm(cluster_centers - point, axis=1)
+        nearest_center_idx = np.argmin(distances)
+
+        # Get its Voronoi neighbors
+        neighbor_indices = list(neighbor_dict[nearest_center_idx])
+
+        # Calculate distances to nearest center and its neighbors
+        relevant_centers = [nearest_center_idx] + neighbor_indices
+        relevant_distances = np.linalg.norm(
+            cluster_centers[relevant_centers] - point[:, np.newaxis].T,
+            axis=1
+        )
+
+        # Convert distances to weights using inverse distance
+        weights = 1 / (relevant_distances + 1e-8)
+        weights = weights / np.sum(weights)
+
+        # Get weighted average of hues from neighboring centers
+        relevant_hues = cluster_hues[relevant_centers]
+
+        # Handle wraparound in hue space by finding the best wrapping
+        # First, center all hues around the nearest center's hue
+        center_hue = relevant_hues[0]
+        wrapped_hues = relevant_hues.copy()
+        for i in range(1, len(wrapped_hues)):
+            diff = wrapped_hues[i] - center_hue
+            if abs(diff) > 0.5:
+                if diff > 0:
+                    wrapped_hues[i] -= 1
+                else:
+                    wrapped_hues[i] += 1
+
+        # Compute weighted average and wrap back to [0,1]
+        interpolated_hue = (np.sum(wrapped_hues * weights)) % 1.0
+
+        return interpolated_hue
+    elif method == 'nearest_two':
         # Get two nearest clusters
         closest_indices = np.argsort(distances)[:2]
         d1, d2 = distances[closest_indices]
@@ -116,6 +184,47 @@ def interpolate_point_hue(point, cluster_centers, cluster_hues, method='nearest_
         interpolated_hue = np.sum(cluster_hues * weights) % 1.0
 
     else:
-        raise Exception("expected method to be 'nearest_two' or 'inverse_distance'")
+        raise Exception("expected method to be 'voronoi_neighbors', 'nearest_two', or 'inverse_distance'")
 
     return interpolated_hue
+
+def main():
+    dim = int(os.environ['DIM'])
+    n_neighbors = int(os.environ['N_NEIGHBORS'])
+
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
+        raise Exception("missing data directory")
+
+    directory = arguments[0]
+    embedding_path = os.path.join(directory, 'graph-emb-{:d}.pkl'.format(dim))
+    label_path = os.path.join(directory, 'graph-label-{:d}-{:d}.pkl'.format(dim, n_neighbors))
+    graph_database_path = os.path.join(directory, 'graph-umap-{:d}-{:d}.sqlite'.format(dim, n_neighbors))
+    atlas_database_path = os.path.join(directory, 'atlas-umap-{:d}-{:d}.sqlite'.format(dim, n_neighbors))
+
+    print("embedding_path", embedding_path)
+    print("label_path", label_path)
+    print("graph_database_path", graph_database_path)
+    print("atlas_database_path", atlas_database_path)
+
+    with open(embedding_path, 'rb') as file:
+        (node_ids, embeddings) = pickle.load(file)
+
+    print("node_ids:", type(node_ids), node_ids.shape)
+    print("embeddings:", type(embeddings), embeddings.shape)
+
+    with open(label_path, 'rb') as file:
+        (node_ids, labels, cluster_centers) = pickle.load(file)
+
+    print("labels:", type(labels), labels.shape)
+    print("cluster_centers:", type(cluster_centers), cluster_centers.shape)
+
+    cluster_hues = derive_cluster_hues(labels=labels, cluster_centers=cluster_centers)
+
+    hues = [interpolate_point_hue(point, cluster_centers, cluster_hues) for point in embeddings]
+
+    save_labels(graph_database_path, node_ids, hues)
+    save_labels(atlas_database_path, node_ids, hues)
+
+if __name__ == "__main__":
+    main()
