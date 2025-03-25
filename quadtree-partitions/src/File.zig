b1640b7c9b27ff58a42663c222f1472f1b0d1ddb
Joel Gustafson <joelgustafson@protonmail.com>
2025-03-25 00:10:57 -0400

    add quadtree-partitions/src/File.zig

---
+++ quadtree-partitions/src/File.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+const File = @This();
+
+data: []align(std.heap.page_size_min) const u8,
+
+pub fn init(path: []const u8) !File {
+    std.log.info("opening {s}", .{path});
+    const fd = try std.posix.open(path, .{}, 644);
+    defer std.posix.close(fd);
+
+    const stat = try std.posix.fstat(fd);
+    const data = try std.posix.mmap(null, @intCast(stat.size), std.posix.PROT.READ, .{ .TYPE = .SHARED }, fd, 0);
+    return File{ .data = data };
+}
+
+pub fn deinit(self: File) void {
+    std.posix.munmap(self.data);
+}
