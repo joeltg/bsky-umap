8a9b8c422de9805ab41ca6f90e4b975deeb220c8
Joel Gustafson <joelgustafson@protonmail.com>
2025-03-31 11:40:35 -0400

    Update quadtree-partitions/lib/lib.zig

--- quadtree-partitions/lib/lib.zig
+++ quadtree-partitions/lib/lib.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const allocator = std.heap.page_allocator;
+const allocator = std.heap.wasm_allocator;
 
 const Atlas = @import("atlas");
 
@@ -20,54 +20,45 @@ pub fn panic(message: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noretu
 
 const Tile = struct {
     area: Atlas.Area,
-    nodes: []const Atlas.Node,
+    nodes: []Atlas.Node,
 };
 
 var tiles = std.ArrayList(Tile).init(allocator);
 
-// export fn init(s: i32, x: i32, y: i32, node_count: u32) ?[*]Atlas.Node {
-export fn init(node_count: u32) ?[*]Atlas.Node {
-    // const area = Atlas.Area{
-    //     .s = @floatFromInt(s),
-    //     .c = .{ @floatFromInt(x), @floatFromInt(y) },
-    // };
-
-    // tiles.append(.{ .area = area, .nodes = nodes }) catch return null;
-    const nodes = allocator.alloc(Atlas.Node, node_count) catch
+export fn tile_init(s: f32, c_x: f32, c_y: f32, node_count: u32) ?*const Tile {
+    const tile = allocator.create(Tile) catch
+        @panic("failed to allocate tile");
+    tile.area.s = s;
+    tile.area.c[0] = c_x;
+    tile.area.c[1] = c_y;
+    tile.nodes = allocator.alloc(Atlas.Node, node_count) catch
         @panic("failed to allocate nodes");
-    return nodes.ptr;
+    return tile;
+}
+
+export fn tile_deinit(tile: *const Tile) void {
+    allocator.free(tile.nodes);
+    allocator.destroy(tile);
+}
+
+export fn tile_nodes_len(tile: *const Tile) usize {
+    return tile.nodes.len;
+}
+
+export fn tile_nodes_ptr(tile: *const Tile) [*]Atlas.Node {
+    return tile.nodes.ptr;
 }
 
 var nearest_body: Atlas.Body = undefined;
 
-export fn get_nearest_body_inclusive(
-    nodes_ptr: [*]const Atlas.Node,
-    nodes_len: usize,
-    area_s: f32,
-    area_x: f32,
-    area_y: f32,
-    x: f32,
-    y: f32,
-) *Atlas.Body {
-    const nodes = nodes_ptr[0..nodes_len];
-    const area = Atlas.Area{ .s = area_s, .c = .{ area_x, area_y } };
-    nearest_body = Atlas.getNearestBody(nodes, area, .{ x, y }, .inclusive) catch
+export fn get_nearest_body_inclusive(tile: *const Tile, x: f32, y: f32) *Atlas.Body {
+    nearest_body = Atlas.getNearestBody(tile.nodes, tile.area, .{ x, y }, .inclusive) catch
         @panic("failed to get nearest body");
     return &nearest_body;
 }
 
-export fn get_nearest_body_exclusive(
-    nodes_ptr: [*]const Atlas.Node,
-    nodes_len: usize,
-    area_s: f32,
-    area_x: f32,
-    area_y: f32,
-    x: f32,
-    y: f32,
-) *Atlas.Body {
-    const nodes = nodes_ptr[0..nodes_len];
-    const area = Atlas.Area{ .s = area_s, .c = .{ area_x, area_y } };
-    nearest_body = Atlas.getNearestBody(nodes, area, .{ x, y }, .exclusive) catch
+export fn get_nearest_body_exclusive(tile: *const Tile, x: f32, y: f32) *Atlas.Body {
+    nearest_body = Atlas.getNearestBody(tile.nodes, tile.area, .{ x, y }, .exclusive) catch
         @panic("failed to get nearest body");
     return &nearest_body;
 }
