66d0c3fa6ad818658baa269ccf7e2ff0c999eea3
Joel Gustafson <joelgustafson@protonmail.com>
2025-10-07 00:58:24 -0400

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+import numpy as np
+from dotenv import load_dotenv
+from numpy.typing import NDArray
+
+from utils import load
+from zumap import plot
+
+# matplotlib.use("TkAgg")
+
+load_dotenv()
+
+if __name__ == "__main__":
+    n_neighbors = int(os.environ["N_NEIGHBORS"])
+    dim = int(os.environ["DIM"])
+
+    metric = os.environ.get("METRIC", "euclidean")
+
+    arguments = sys.argv[1:]
+    if len(arguments) == 0:
+        raise Exception("missing data directory")
+
+    directory = arguments[0]
+
+    positions: NDArray[np.float32] = load(
+        directory, f"positions-{dim}-{metric}-{n_neighbors}.npy"
+    )
+
+    min_dist = 0.1
+    n_neighbors = 10
+
+    axis = plot.points(
+        positions, n_neighbors=n_neighbors, min_dist=min_dist, theme="viridis"
+    )
+    plot.show(axis)
