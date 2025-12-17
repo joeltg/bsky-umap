581610871037d832d4f84ec229f7160a673285bb
Joel Gustafson <joelgustafson@protonmail.com>
2025-12-17 17:18:06 -0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     directory = arguments[0]
 
-    weights = load(directory, "weights.npy")
     sources = load(directory, "sources.npy")
     targets = load(directory, "targets.npy")
 
@@ -18,13 +17,11 @@ if __name__ == "__main__":
         {
             "sources": sources,
             "targets": targets,
-            "weights": weights,
         }
     )
     df = df.sort(["sources", "targets"])
 
     print("Edges sorted!")
 
-    save(directory, "weights.npy", df["weights"].to_numpy())
     save(directory, "sources.npy", df["sources"].to_numpy())
     save(directory, "targets.npy", df["targets"].to_numpy())
