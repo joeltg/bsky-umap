ab408e978b1f3b7de1ed1778f80a4f77bc3257a3
Joel Gustafson <joelgustafson@protonmail.com>
2025-12-17 21:16:56 -0500

    swap names

--- save_arrow.py
+++ save_arrow.py
@@ -4,8 +4,8 @@ import sys
 import pyarrow as pa
 import vortex as vx
 
-# import matplotlib.pyplot as plt
-from utils import load
+
+from utils import load, write_edges, write_nodes
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
@@ -15,53 +15,12 @@ if __name__ == "__main__":
     incoming_degrees = load(directory, "incoming_degrees.npy")
     outgoing_degrees = load(directory, "outgoing_degrees.npy")
 
-
-
-    node_data = vx.Array.from_arrow(
-        pa.StructArray.from_arrays(
-            arrays=[ids, incoming_degrees, outgoing_degrees],
-            fields=[
-                pa.field("ids", pa.uint32(), nullable=False),
-                pa.field("incoming_degrees", pa.uint32(), nullable=False),
-                pa.field("outgoing_degrees", pa.uint32(), nullable=False),
-            ],
-        ),
-    )
-
-    assert node_data.dtype == vx.struct(
-        {
-            "ids": vx.uint(32),
-            "incoming_degrees": vx.uint(32),
-            "outgoing_degrees": vx.uint(32),
-        }
-    )
-
-    node_output_path = os.path.join(directory, "nodes.vortex")
-    vx.io.write(node_data, node_output_path)
+    nodes_path = os.path.join(directory, "nodes.arrow")
+    write_nodes(nodes_path, ids, incoming_degrees, outgoing_degrees)
 
     weights = load(directory, "weights.npy")
     sources = load(directory, "sources.npy")
     targets = load(directory, "targets.npy")
 
-
-    edge_data = vx.Array.from_arrow(
-        pa.StructArray.from_arrays(
-            arrays=[weights, sources, targets],
-            fields=[
-                pa.field("weights", pa.float32(), nullable=False),
-                pa.field("sources", pa.int32(), nullable=False),
-                pa.field("targets", pa.int32(), nullable=False),
-            ],
-        ),
-    )
-
-    assert edge_data.dtype == vx.struct(
-        {
-            "weights": vx.float_(32),
-            "sources": vx.int_(32),
-            "targets": vx.int_(32),
-        }
-    )
-
-    edge_output_path = os.path.join(directory, "edges.vortex")
-    vx.io.write(edge_data, edge_output_path)
+    edges_path = os.path.join(directory, "edges.arrow")
+    write_edges(edges_path, (weights, rows, cols))
